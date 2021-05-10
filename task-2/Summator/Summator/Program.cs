3a5db0b36c8c7a6b0d20a6ec6b0c47b05c91c1cd
Evgeniy <uon.St1ke@mail.ru>
2021-05-11 05:21:53 +0700

    Создано консольное приложение

---
+++ task-2/Summator/Summator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
