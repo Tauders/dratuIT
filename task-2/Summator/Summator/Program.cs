9491c7139a8d7669232ef38d8474795b3f58d960
Evgeniy <uon.St1ke@mail.ru>
2021-05-13 13:21:44 +0700

    [task-3 Summator] Добавил в код обработку исключения
    Или показалось слишком простым заданием и конкретно тут смысл таков, что пользователь использующий данную программу получал информацию о том что именно сделал не так, или я не понял задачу. Я решил добавить вторую попытку для пользователя, знаю можно сделать более совершенно это циклами в будущем, но пока так.

--- task-2/Summator/Summator/Program.cs
+++ task-2/Summator/Summator/Program.cs
@@ -8,12 +8,27 @@ namespace Summator
         {
             double a;
             double b;
+            try
+            {
 
-            Console.WriteLine("Введите пару чисел для их сложения");
-            a = Convert.ToDouble(Console.ReadLine());
-            b = Convert.ToDouble(Console.ReadLine());
-            double total = a + b;
-            Console.WriteLine($"Сумма {a} и {b} равна {total}.");
+                Console.WriteLine("Введите пару чисел для их сложения");
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                double total = a + b;
+                Console.WriteLine($"Сумма {a} и {b} равна {total}.");
+            }
+            catch
+            {
+                Console.WriteLine("Введите числовые значения.");
+            }
+            finally
+            {
+
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                double total = a + b;
+                Console.WriteLine($"Сумма {a} и {b} равна {total}.");
+            }
         }
     }
 }
