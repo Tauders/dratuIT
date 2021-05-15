89ad5c3dd96b61898cd04c8476a7a895bf74f3b1
morgunovdv <morgunovdv3@hotmail.com>
2021-05-15 14:36:28 +0700

    [task-1] Переделал через enum список фигур

--- task-1/WorkwithFiles/CreateFiles/Program.cs
+++ task-1/WorkwithFiles/CreateFiles/Program.cs
@@ -4,79 +4,78 @@ namespace CreateFiles
 {
     class Program
     {
-        //Куб
-        //Цилиндр
-        //Пирамида
-        //Шар
-        //Конус
-        //Призма
+        enum Figures
+        {
+            Cube,
+            Cylinder,
+            Pyramid,
+            Ball,
+            Cone,
+            Prism
+        } 
 
-        static private int SelectFigure()
+        static private Figures SelectFigure()
         {
             Random rdn = new Random();
-            int selectFugure = rdn.Next(1, 7);
-            return selectFugure;
+            Figures figures = (Figures)rdn.Next(6);
+            return figures;
         }
 
-        static private int CreateFigure()
+        static private object CreateFigure(Figures figures)
         {
-            
+            Random rdnSize = new Random();
+            double h = 11.0 + rdnSize.NextDouble() * (11.0 - 1.0);
+            double r = 11.0 + rdnSize.NextDouble() * (11.0 - 1.0);
+            double s = 11.0 + rdnSize.NextDouble() * (11.0 - 1.0);
 
-            Random rdn = new Random();
-            double h = 11.0 + rdn.NextDouble() * (11.0 - 1.0);
-            double r = 11.0 + rdn.NextDouble() * (11.0 - 1.0);
-            double s = 11.0 + rdn.NextDouble() * (11.0 - 1.0);
-            int figure = SelectFigure();
-
-            switch (figure)
+            switch (figures)
             {
-                case 1:
+                case Figures.Cube:
                     Cube cube = new Cube(r);
                     Console.WriteLine($"Созданная вами фигура это {cube} и её объем {cube.Volume()}");
                     break;
-                case 2:
+                case Figures.Cylinder:
                     Cylinder cylinder = new Cylinder(h, r);
                     Console.WriteLine($"Созданная вами фигура это {cylinder} и её объем {cylinder.Volume()}");
                     break;
-                case 3:
+                case Figures.Pyramid:
                     Pyramid pyramid = new Pyramid(s, r);
                     Console.WriteLine($"Созданная вами фигура это {pyramid} и её объем {pyramid.Volume()}");
                     break;
-                case 4:
+                case Figures.Ball:
                     Ball ball = new Ball(r);
                     Console.WriteLine($"Созданная вами фигура это {ball} и её объем {ball.Volume()}");
                     break;
-                case 5:
+                case Figures.Cone:
                     Cone cone = new Cone(h, r);
                     Console.WriteLine($"Созданная вами фигура это {cone} и её объем {cone.Volume()}");
                     break;
-                case 6:
+                case Figures.Prism:
                     Prism prism = new Prism(h);
                     Console.WriteLine($"Созданная вами фигура это {prism} и её объем {prism.Volume()}");
                     break;
             }
-
-            
-            return figure;
+            Console.WriteLine(figures);
+            return figures;
         }
 
         static void Main(string[] args)
         {
             Random rnd = new Random();
-
-            int n = rnd.Next(1,11);
-
-            
+            int n = rnd.Next(1, 11);
 
             object[] vars = new object[n];
-            for (int i = 0; i<vars.Length; i++)
+            for (int i = 0; i < vars.Length; i++)
             {
-                vars[i] = CreateFigure();
+                vars[i] = CreateFigure(SelectFigure());
             }
             Console.WriteLine(vars);
             Console.ReadKey();
 
+
+
+            
+
         }
-        
     }
 }
