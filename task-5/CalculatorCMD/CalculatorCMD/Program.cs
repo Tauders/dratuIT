3a94b8ed171679daa546c4223e3e94fb0c413e4b
Evgeniy <uon.St1ke@mail.ru>
2021-05-19 18:58:37 +0700

    [task-5] WIP Создание консольного приложения CalculatorCMD

--- task-5/CalculatorCMD/CalculatorCMD/Program.cs
+++ task-5/CalculatorCMD/CalculatorCMD/Program.cs
@@ -6,7 +6,46 @@ namespace CalculatorCMD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                double a = double.Parse(args[0]);
+                double b = double.Parse(args[1]);
+                char oper = char.Parse(args[2]);
+            
+                double total;
+
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine(total);
+                }
+
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine(total);
+                }
+
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine(total);
+                }
+
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine(total);
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестный оператор");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Введены некорректные символы, пожалуйста попробуйте ещё раз, используя подобную форму ввода: число число операция. Полученная ошибка: {ex.Message}");
+            }
         }
     }
 }
