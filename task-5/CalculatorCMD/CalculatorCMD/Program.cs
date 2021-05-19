c1638f6c6ca8e083d13f9b6fbce97a77d3ff637a
Evgeniy <uon.St1ke@mail.ru>
2021-05-19 20:47:09 +0700

    [task-5]Исправление ошибок консольного приложения CalculatorCMD

--- task-5/CalculatorCMD/CalculatorCMD/Program.cs
+++ task-5/CalculatorCMD/CalculatorCMD/Program.cs
@@ -6,46 +6,47 @@ namespace CalculatorCMD
     {
         static void Main(string[] args)
         {
+            double number1;
+            double number2;
+            char action;
             try
             {
-                double a = double.Parse(args[0]);
-                double b = double.Parse(args[1]);
-                char oper = char.Parse(args[2]);
-            
-                double total;
+                number1 = double.Parse(args[0]);
+                number2 = double.Parse(args[1]);
+                action = char.Parse(args[2]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Введены некорректные символы, пожалуйста попробуйте ещё раз, используя подобную форму ввода: число число операция. Полученная ошибка: {ex.Message}");
+                return;
+            }
 
-                if (oper == '+')
+            double total;
+
+                if (action == '+')
                 {
-                    total = a + b;
+                    total = number1 + number2;
                     Console.WriteLine(total);
                 }
-
-                else if (oper == '-')
+                else if (action == '-')
                 {
-                    total = a - b;
+                    total = number1 - number2;
                     Console.WriteLine(total);
                 }
-
-                else if (oper == '*')
+                else if (action == '*')
                 {
-                    total = a * b;
+                    total = number1 * number2;
                     Console.WriteLine(total);
                 }
-
-                else if (oper == '/')
+                else if (action == '/')
                 {
-                    total = a / b;
+                    total = number1 / number2;
                     Console.WriteLine(total);
                 }
                 else
                 {
                     Console.WriteLine("Неизвестный оператор");
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Введены некорректные символы, пожалуйста попробуйте ещё раз, используя подобную форму ввода: число число операция. Полученная ошибка: {ex.Message}");
-            }
         }
     }
 }
