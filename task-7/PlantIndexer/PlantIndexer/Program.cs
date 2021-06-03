282ef928a54c0c16b7cc7310a7e64723f84817d6
Evgeniy <uon.St1ke@mail.ru>
2021-06-03 19:03:01 +0700

    [task-7] Создал Основу для консольного приложения PlantIndexer

---
+++ task-7/PlantIndexer/PlantIndexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlantIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
