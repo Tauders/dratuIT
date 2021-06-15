505413ade5e7861cd988e00360f6dc48bdb95e73
morgunovdv <morgunovdv3@hotmail.com>
2021-06-15 11:44:25 +0700

    [task - 1] Добавил сериализацию Json

--- task-1/WorkwithFiles/CreateFiles/Program.cs
+++ task-1/WorkwithFiles/CreateFiles/Program.cs
@@ -21,24 +21,54 @@ namespace CreateFiles
 
         private static void XmlSerializeFigur(string path)
         {
+            Console.WriteLine("Сериализация");
             Shape[] shape = CreatFigure.GetShapes();
             XmlSerializer serializer = new XmlSerializer(typeof(Shape[]));
-            using (FileStream fs = new FileStream(path, FileMode.Create))
+            using (FileStream fs = new FileStream((path + ".xml"), FileMode.Create))
             {
                 serializer.Serialize(fs, shape);
             }
+            Console.WriteLine("===================");
         }
 
         private static void XmlDeserializaFigur(string path)
         {
+            Console.WriteLine("Десериализация");
             XmlSerializer serializer = new XmlSerializer(typeof(Shape[]));
-            using (FileStream fs = new FileStream(path, FileMode.Open))
+            using (FileStream fs = new FileStream(path + ".xml", FileMode.Open))
             {
                 Shape[] shapes = (Shape[])serializer.Deserialize(fs);
                 foreach (Shape shape in shapes)
                 {
-                    Console.WriteLine($"Фигура {shape.Name} и её объём {shape.ShapeVolume},");
+                    Console.WriteLine($"Фигура {shape.Name} и её объём {shape.ShapeVolume}");
                 }
+            }
+            Console.WriteLine("===================");
+        }
+
+        private static async void  JsonSerializeFigur(string path)
+        {
+            Console.WriteLine("Сериализация");
+            using (FileStream fs = new FileStream((path + ".json"), FileMode.Create))
+            {
+                Shape[] shapes = CreatFigure.GetShapes();
+                await JsonSerializer.SerializeAsync<Shape[]>(fs, shapes);
+                Console.WriteLine("Файл создан");
+            }
+            Console.WriteLine("===================");
+        }
+
+        private static async void JsonDeserializaFigur(string path)
+        {
+            Console.WriteLine("Десериализация");
+            using (FileStream fs = new FileStream((path + ".json"), FileMode.Open))
+            {
+                Shape[] shapes = await JsonSerializer.DeserializeAsync<Shape[]>(fs);
+                foreach (Shape shape in shapes)
+                {
+                    Console.WriteLine($"Фигура {shape.Name} и её объём {shape.ShapeVolume}");
+                }
+                Console.WriteLine("===================");
             }
         }
 
@@ -50,10 +80,15 @@ namespace CreateFiles
             string fileName = Console.ReadLine();
             string path = CreateDirectory(directory, fileName);
             Console.Clear();
-            //Encoding utf8 = Encoding.UTF8;
-            XmlSerializeFigur(path);
+            ////Encoding utf8 = Encoding.UTF8;
+            //Console.WriteLine("Работа с файлами типа XML");
+            //XmlSerializeFigur(path);
+            //XmlDeserializaFigur(path);
+
+            Console.WriteLine("Работа с файлами типа JSON");
+            JsonSerializeFigur(path);
             Console.WriteLine("===================");
-            XmlDeserializaFigur(path);
+            JsonDeserializaFigur(path);
 
             Console.ReadKey();
         }
