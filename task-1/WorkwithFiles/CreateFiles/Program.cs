35b44ead15d7c53ddead199d6639e7b62f2bd5e1
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-07-30 20:25:20 +0700

    [task - 1] Приступил к основной задаче с теми файлами которые уже могу десериализовать

--- task-1/WorkwithFiles/CreateFiles/Program.cs
+++ task-1/WorkwithFiles/CreateFiles/Program.cs
@@ -11,45 +11,43 @@ namespace CreateFiles
 {
     class Program
     {
-        private static void CsvDeserializeFigur(Shape[] shapes, string path, CsvConfiguration config)
-        {
-            Console.WriteLine("Десериализация");
-            using (StreamReader sr = new StreamReader(path + ".csv"))
-            {
-                using (CsvReader csv = new CsvReader(sr, config))
-                {
-                    StringBuilder st = new StringBuilder();
-                    st.AppendLine("Name, Radius, Height, Area");
-                    foreach (Shape shape in shapes) ;
-
-                }
-            }
-        }
+        //private static Shape[] CsvDeserializeFigur(Shape[] shapes, string path, CsvConfiguration config)
+        //{
+        //    Console.WriteLine("Десериализация");
+        //    using (StreamReader sr = new StreamReader(path + ".csv"))
+        //    {
+        //        using (CsvReader csv = new CsvReader(sr, config))
+        //        {
+        //            var shapes1 = csv.GetRecords<Shape[]>();
+        //            return shapes1;
+        //        }
+        //    }
+        //}
 
 
         private static void Main(string[] args)
         {
-            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                Encoding = Encoding.UTF8,
-                IgnoreBlankLines = false,
-                HasHeaderRecord = false,
-                Delimiter = ";"
-            };
-            //JsonSerializerSettings jset = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Objects };
+            ////CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            ////{
+                
+            ////    Encoding = Encoding.UTF8,
+            ////    IgnoreBlankLines = false,
+            ////    HasHeaderRecord = true,
+            ////    Delimiter = ";"
+            ////};
+            JsonSerializerSettings jset = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Objects };
             //Console.WriteLine("Введите название файла");
             string fileName = "tester"; //Console.ReadLine();
             string path = Serialization.CreateDirectory(@"D:/Figures", fileName);
             Shape[] shapes = CreateFigure.GetShapes();
-            //Console.WriteLine("Работа с файлами типа XML");
-            //Serialization.XmlSerializeFigur(path, shapes);
-            //Shape[] XmlShapes = Deserialization.XmlDeserializeFigur(path);
-            //Console.WriteLine("Работа с файлами типа JSON");
-            //Serialization.JsonSerializeFigur(path, shapes, jset);
-            //Shape[] JsonShapes = Deserialization.JsonDeserializeFigur(path, jset);
-            Console.WriteLine("Работа с файлами типа CSV");
-            Serialization.CsvSerializeFigur(path, shapes, config);
-            //CsvDeserializeFigur(path, config);
+            Console.WriteLine("Работа с файлами типа XML");
+            Serialization.XmlSerializeFigur(path, shapes);
+            Console.WriteLine("Работа с файлами типа JSON");
+            Serialization.JsonSerializeFigur(path, shapes, jset);
+            //Console.WriteLine("Работа с файлами типа CSV");
+            //Serialization.CsvSerializeFigur(path, shapes, config);
+            //Shape[] csvshape = CsvDeserializeFigur(shapes ,path, config);
+            //Console.WriteLine(csvshape);
 
             Console.ReadKey();
         }
--- task-1/WorkwithFiles/WorkwithFiles/Program.cs
+++ task-1/WorkwithFiles/WorkwithFiles/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using LibShapes;
+using Newtonsoft.Json;
 
 namespace WorkwithFiles
 {
@@ -7,7 +8,32 @@ namespace WorkwithFiles
     {
         static void Main(string[] args)
         {
-            
+            string fileName = "tester"; //Console.ReadLine();
+            string path = Serialization.CreateDirectory(@"D:/Figures", fileName);
+            JsonSerializerSettings jset = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Objects };
+            Shape[] Jsonshapes = Deserialization.JsonDeserializeFigur(path, jset);
+            Shape[] Xmlshapes = Deserialization.XmlDeserializeFigur(path);
+            double xmlresult = 0;
+            double jsonresult = 0;
+
+            for (int i = 0; i<Xmlshapes.Length; i++)
+            {
+                xmlresult += Xmlshapes[i].Volume();
+                Console.WriteLine(Xmlshapes[i].Volume());
+                
+            }
+            Console.WriteLine(xmlresult);
+            Console.WriteLine("=======================================");
+            for (int i = 0; i < Jsonshapes.Length; i++)
+            {
+                jsonresult += Jsonshapes[i].Volume();
+                Console.WriteLine(Jsonshapes[i].Volume());
+
+            }
+            Console.WriteLine(jsonresult);
+
+
+            Console.ReadKey();
         }
     }
 }
