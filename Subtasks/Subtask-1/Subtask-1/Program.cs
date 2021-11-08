80d6bf41fe8933adbfd1b0c286ad294957237103
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-11-08 20:47:50 +0700

    [Subtask-1] Решение первой задачи

---
+++ Subtasks/Subtask-1/Subtask-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Subtask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк, которые хотите использовать");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] words = new string[n];
+            int i = 0;
+            while (i < words.Length)
+            {
+                words[i] = Console.ReadLine();
+                i++;
+            }
+            Console.WriteLine("Все строки введены, выберите строку для отображения");
+            bool b = true;
+            while (b == true)
+            {
+                string word = Console.ReadLine();
+                if (word != "X")
+                {
+                    int index = Convert.ToInt32(word);
+                    if (index < words.Length)
+                    {
+                        Console.WriteLine(words[index]);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Ошибка");
+                    }
+                }
+
+                else
+                {
+                    b = false;
+                }
+            }
+
+            Console.WriteLine("Работа завершена");
+
+            Console.ReadKey();
+        }
+    }
+}
+
