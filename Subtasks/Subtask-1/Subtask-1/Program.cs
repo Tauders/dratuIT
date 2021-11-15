b020c59277a768729406f12b8bdc484228bc696e
morgunovdv <morgunovdv3@hotmail.com>
2021-11-15 14:37:55 +0700

    [Subtask-1] Добавил обработку исключений.

--- Subtasks/Subtask-1/Subtask-1/Program.cs
+++ Subtasks/Subtask-1/Subtask-1/Program.cs
@@ -1,49 +1,68 @@
 ﻿using System;
-
 namespace Subtask_1
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите количество строк, которые хотите использовать");
-            int n = Convert.ToInt32(Console.ReadLine());
-            string[] words = new string[n];
-            int i = 0;
-            while (i < words.Length)
+            int n;
+            while (true)
             {
-                words[i] = Console.ReadLine();
-                i++;
-            }
-            Console.WriteLine("Все строки введены, выберите строку для отображения");
-            bool b = true;
-            while (b == true)
-            {
-                string word = Console.ReadLine();
-                if (word != "X")
+                Console.WriteLine("Введите количество строк, которые хотите использовать");
+                string input = Console.ReadLine();
+                if (Int32.TryParse(input, out n))
                 {
-                    int index = Convert.ToInt32(word);
-                    if (index < words.Length)
-                    {
-                        Console.WriteLine(words[index]);
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("Ошибка");
-                    }
+                    break;
                 }
 
                 else
                 {
-                    b = false;
+                    Console.WriteLine("Введите числовое значение");
+                }
+            }
+
+            string[] words = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Все строки введены, выберите строку для отображения");
+
+            while (true)
+            {
+                try
+                {
+                    string value = Console.ReadLine();
+                    if (value != "X")
+                    {
+                        int index = Convert.ToInt32(value);
+                        if (index < n)
+                        {
+                            Console.WriteLine(words[index]);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ошибка");
+                        }
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Указаны не верные данные");
                 }
             }
 
             Console.WriteLine("Работа завершена");
-
             Console.ReadKey();
-        }
+            }
     }
 }
 
