a4435c8abd3a987ddb74ffff8808ddb14cb770c8
Evgeniy <uon.St1ke@mail.ru>
2021-11-16 00:04:03 +0700

    [Subtask-2] Начало работы

---
+++ Subtasks/Evgeniy/Subtask-2/Subtask-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Subtask_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(3, 7);
+            Console.WriteLine($"Программа сгенерировала случайное число, {value}");
+            string[] chisla = new string[value];
+            string s = Console.ReadLine();
+            chisla = s.Split();
+            int a;
+            for(a = 0; a < value; a++)
+            {
+                a = Convert.ToInt32(chisla);
+            }
+            Console.WriteLine($"{a}");
+
+            Console.WriteLine("Числа введены выберите доступные");
+        }
+    }
+}
