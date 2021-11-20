864fa1597de4442e92310bc29c1b0516b0cab6e2
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-11-20 23:13:10 +0700

    [Subtask - 2] Устранил недочеты

--- Subtasks/Denis/Subtask-2/Program.cs
+++ Subtasks/Denis/Subtask-2/Program.cs
@@ -39,7 +39,6 @@ namespace Subtask_2
                         {
                             Console.WriteLine("Делить на 0 нельзя");
                         }
-
                         else
                         {
                             result /= valuesOfNumbers[i];
@@ -61,17 +60,16 @@ namespace Subtask_2
             Random rnd = new Random();
             int n = rnd.Next(3, 7);
             int[] numbers = new int[n];
+            
             Console.WriteLine($"Программа сгенерировала список из {n} чисел");
-
-            int number = 0;
             for (int i = 0; i < numbers.Length; i++)
             {
+                int number = 0;
                 string inputNumber = Console.ReadLine();
                 if (Int32.TryParse(inputNumber, out number))
                 {
-                    numbers[i] = number; ;
+                    numbers[i] = number;
                 }
-
                 else
                 {
                     Console.WriteLine("Некорректный ввод");
@@ -81,7 +79,8 @@ namespace Subtask_2
                     }
                 }
             }
-
+            
+            
             Console.Write($"Числа введены, выберите доступные: ");
             for (int i =0; i < numbers.Length; i++)
             {
@@ -89,22 +88,21 @@ namespace Subtask_2
             }
 
             Console.WriteLine();
-            int index = 0;
+            
             List<int> valuesOfNumbers = new List<int>();
             while (true)
             {
                 string input = Console.ReadLine();
+                int index = 0;
                 if ((Int32.TryParse(input, out index)) && (index < numbers.Length))
                 {
                     Console.WriteLine($"Выбранное вами число: {numbers[index]}");
                     valuesOfNumbers.Add(numbers[index]);
                 }
-
                 else if (input == "X")
                 {
                     break;
                 }
-
                 else
                 {
                     Console.WriteLine("Некорректный ввод");
@@ -121,10 +119,9 @@ namespace Subtask_2
                     Console.WriteLine($"Действие выбрано, результат - {result}");
                     break;
                 }
-
                 else
                 {
-                    Console.WriteLine("Действие не выбрано");
+                    Console.WriteLine("Данное действие не поддерживается");
                 }
             }
             Console.WriteLine("Завершение работы");
