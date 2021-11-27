0ca16c65866b20782222d81e139088401bab5736
morgunovdv <morgunovdv3@hotmail.com>
2021-11-27 10:54:15 +0700

    [Subtask - 3] Убрал break

--- Subtasks/Denis/Subtask-3/Subtask-3/Program.cs
+++ Subtasks/Denis/Subtask-3/Subtask-3/Program.cs
@@ -7,42 +7,32 @@ namespace Subtask_3
     {
         static void Main(string[] args)
         {
+            string input = null;
             List<string> words = new List<string>();
             Console.WriteLine("Введите любое количество слов, для остановки введите X");
-            while (true)
+            while (input != "X")
             {
-                string word = Console.ReadLine();
-                if (word != "X")
-                {
-                    words.Add(word);
-                }
-                else
-                {
-                    break;
-                }
+                input = Console.ReadLine();
+                words.Add(input);
             }
             Console.WriteLine("Данные введены, выберите способ сортировки (asc, desc)");
             string method = Console.ReadLine();
             if (method == "asc")
             {
                 words.Sort();
-                foreach (string word in words)
-                {
-                    Console.WriteLine(word);    
-                }
             }
             else if (method == "desc")
             {
                 words.Sort();
                 words.Reverse();
-                foreach (string word in words)
-                {
-                    Console.WriteLine(word);
-                }
             }
             else
             {
                 Console.WriteLine("Данное действие не поддерживается");
+            }
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
             }
 
             Console.WriteLine("Все значения выведены");
