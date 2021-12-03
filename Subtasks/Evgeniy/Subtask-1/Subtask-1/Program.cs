20f3377e67fca8c8b5b3636727e93e03785bd14a
Evgeniy <uon.St1ke@mail.ru>
2021-12-03 18:31:24 +0700

    [Subtask-1] Исправление недочётов
    Сделал наименование переменных более осмысленными

--- Subtasks/Evgeniy/Subtask-1/Subtask-1/Program.cs
+++ Subtasks/Evgeniy/Subtask-1/Subtask-1/Program.cs
@@ -7,39 +7,39 @@ namespace Subtask_1
         static void Main(string[] args)
         {
             Console.WriteLine("Введите число строк");
-            string g = Console.ReadLine();
-            int N;
+            string numberOfStringsUserInput = Console.ReadLine();
+            int numberOfStrings;
             while (true)
             {
-                bool check = int.TryParse(g, out N);
-                if (check == true && N >= 0)
+                bool check = int.TryParse(numberOfStringsUserInput, out numberOfStrings);
+                if (check == true && numberOfStrings >= 0)
                 {
                     
                     break;
                 }
                 else
                 {
-                    Console.WriteLine("Ошибка, введите число");
-                    g = Console.ReadLine();
+                    Console.WriteLine("Ошибка, введите положительное число");
+                    numberOfStringsUserInput = Console.ReadLine();
                 }
             }
-            string[] items = new string[N];
+            string[] items = new string[numberOfStrings];
             Console.WriteLine("Введите строки количество которых указали ранее");
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < numberOfStrings; i++)
             {
                 items[i] = Console.ReadLine();
             }
             Console.WriteLine("Все строки введены, выберите строку для отображения");
-            string s = Console.ReadLine();
-            while (s != "X")
+            string choosenStringToShow = Console.ReadLine();
+            while (choosenStringToShow != "X")
             {
-                int a;
-                bool result = int.TryParse(s, out a);
-                if (result == true && a < N && a >= 0)
+                int selectedElementOfArray;
+                bool result = int.TryParse(choosenStringToShow, out selectedElementOfArray);
+                if (result == true && selectedElementOfArray < numberOfStrings && selectedElementOfArray >= 0)
                 {
-                    Console.WriteLine(items[a]);
+                    Console.WriteLine(items[selectedElementOfArray]);
                 }
-                else if (a >= N)
+                else if (selectedElementOfArray >= numberOfStrings)
                 {
                     Console.WriteLine("Вы ввели неверный номер элемента массива");
                 }
@@ -47,7 +47,7 @@ namespace Subtask_1
                 {
                     Console.WriteLine("Ошибка! Не найден индекс массива");
                 }
-                s = Console.ReadLine();
+                choosenStringToShow = Console.ReadLine();
             }
             Console.WriteLine("Программа завершается");
         }
