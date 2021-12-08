37637e37fae9274b1bf3b54ea183c8200aa05a19
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-12-08 23:06:25 +0700

    [Subtask - 3] Финальная версия

--- Subtasks/Denis/Subtask-3/Subtask-3/Program.cs
+++ Subtasks/Denis/Subtask-3/Subtask-3/Program.cs
@@ -3,38 +3,61 @@ using System.Collections.Generic;
 
 namespace Subtask_3
 {
+    
     internal class Program
     {
+        public static List<string> GetSort(List<string> words, string method)
+        {
+            switch (method)
+            {
+                case "asc":
+                    words.Sort();
+                    break;
+                case "desc":
+                    words.Sort();
+                    words.Reverse();
+                    break;
+                default:
+                    break;
+            }
+            return words;
+        }
         static void Main(string[] args)
         {
-            string input = null;
+            bool isExit = false;
             List<string> words = new List<string>();
             Console.WriteLine("Введите любое количество слов, для остановки введите X");
-            while (input != "X")
+            while (!isExit)
             {
-                input = Console.ReadLine();
-                words.Add(input);
+                string input = Console.ReadLine();
+                if (input == "X")
+                {
+                    isExit = true;
+                    Console.WriteLine("Данные введены, выберите способ сортировки (asc, desc)");
+                }
+                else
+                {
+                    words.Add(input);
+                }
             }
-            Console.WriteLine("Данные введены, выберите способ сортировки (asc, desc)");
-            string method = Console.ReadLine();
-            if (method == "asc")
+            bool isSelect = false;
+            while (!isSelect)
             {
-                words.Sort();
-            }
-            else if (method == "desc")
-            {
-                words.Sort();
-                words.Reverse();
-            }
-            else
-            {
-                Console.WriteLine("Данное действие не поддерживается");
+                string method = Console.ReadLine();
+                if ((method == "asc") || (method == "desc"))
+                {
+                    GetSort(words, method);
+                    isSelect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Данное действие не поддерживается");
+                }
             }
             foreach (string word in words)
             {
                 Console.WriteLine(word);
             }
-
             Console.WriteLine("Все значения выведены");
             Console.ReadKey();
         }
