e567544cd2661697b4f8db8f8490a8591ed6d916
Denis Morgunov <morgunovdv3@hotmail.com>
2021-12-09 14:23:41 +0700

    [Subtask - 4] Формирую основу задачи

---
+++ Subtasks/Denis/Subtask-4/Subtask-4/Sport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subtask_4
+{
+    internal class Sport
+    {
+    }
+}
