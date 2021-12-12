f7ed0d0283ba32481e802d29d5ae9e1b8588ddce
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-12-12 17:25:56 +0700

    Revert "[Subtask - 2] Повтороное удаление лишнего из этой ветки"
    This reverts commit 925e3f3ab319c625ee96f8624e30d60c36a71bcd.

---
+++ Subtasks/Denis/Subtask-4/Subtask-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subtask_4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Group, Person> groups = new Dictionary<Group, Person>();
+            bool isExit = false;
+            Console.WriteLine("Введите данные в формате \"имя группа\", имя и группа  - одно слово ");
+            while (!isExit)
+            {
+                string input = Console.ReadLine();
+                string[] temp = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                //Sport sport = new Sport();
+                //Person person = new Person();
+                if (input == "X")
+                {
+                    isExit = true;
+                }
+                else
+                {
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Group group = new Group();
+                            group.Name = temp[i];
+                        }
+                        else
+                        {
+                            Person person = new Person();
+                            person.Name = temp[i];
+                        }
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine(groups.Count);
+            foreach (Person name in groups.Values)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
