692d9d7378d153723d488549c9634a87e7aa05e8
Evgeniy <uon.St1ke@mail.ru>
2022-01-20 02:49:08 +0700

    [Subtask - 2]WIP Написал логику, требуется доработка исключений итд

--- Subtasks/Evgeniy/Subtask-2/Subtask-2/Program.cs
+++ Subtasks/Evgeniy/Subtask-2/Subtask-2/Program.cs
@@ -8,18 +8,57 @@ namespace Subtask_2
         {
             Random rnd = new Random();
             int value = rnd.Next(3, 7);
+            int[] convertedNumbers = new int[value];
             Console.WriteLine($"Программа сгенерировала случайное число, {value}");
-            string[] chisla = new string[value];
+            string[] numbers = new string[value];
             string s = Console.ReadLine();
-            chisla = s.Split();
-            int a;
-            for(a = 0; a < value; a++)
+            numbers = s.Split(new char[] { ' ' });
+            int i = 0;
+            for (i = 0; i < value; i++)
             {
-                a = Convert.ToInt32(chisla);
+                convertedNumbers[i] = Convert.ToInt32(numbers[i]);
             }
-            Console.WriteLine($"{a}");
-
-            Console.WriteLine("Числа введены выберите доступные");
+            Console.WriteLine("Числа ввведены, выберите нужное из доступных. X - выход");
+            int[] choosenNumbers = new int[value];
+            int k = 0;
+            while (Console.ReadLine() != "X")
+            {
+                string j = Console.ReadLine();
+                choosenNumbers[k] = convertedNumbers[Convert.ToInt32(j)];
+                k++;
+            }
+            Console.WriteLine("Числа выбраны, введите необходимое действие");
+            string actions = Console.ReadLine();
+            int total = choosenNumbers[0];
+            if (actions == "+")
+            {
+                for (i = 1; i <= k; i++)
+                {
+                    total += choosenNumbers[i];
+                }
+            }
+            else if (actions == "-")
+            {
+                for(i = 1; i <= k; i++)
+                {
+                    total -= choosenNumbers[i];
+                }
+            }
+            else if (actions == "/")
+            {
+                for (i = 1; i <= k; i++)
+                {
+                    total /= choosenNumbers[i];
+                }
+            }
+            else if (actions == "*")
+            {
+                for (i = 1; i <= k; i++)
+                {
+                    total *= choosenNumbers[i];
+                }
+            }
+            Console.WriteLine(total);
         }
     }
 }
