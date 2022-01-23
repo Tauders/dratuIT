3a71891e8b9345a9ee3eb7747071c791978f5407
Evgeniy <uon.St1ke@mail.ru>
2022-01-24 01:21:33 +0700

    [Subtask-2] WIP Исправление кода
    1)Добавил исключения
    2)Исправил несколько логических ошибок

--- Subtasks/Evgeniy/Subtask-2/Subtask-2/Program.cs
+++ Subtasks/Evgeniy/Subtask-2/Subtask-2/Program.cs
@@ -14,46 +14,75 @@ namespace Subtask_2
             string s = Console.ReadLine();
             numbers = s.Split(new char[] { ' ' });
             int i = 0;
-            for (i = 0; i < value; i++)
+            while (i != (value - 1))
             {
-                convertedNumbers[i] = Convert.ToInt32(numbers[i]);
+                bool firstSuccess = int.TryParse(numbers[i], out int newNumbers);
+                if (firstSuccess)
+                {
+                    convertedNumbers[i] = newNumbers;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод: Введите числовые значения");
+                    s = Console.ReadLine();
+                    numbers = s.Split(new char[] { ' ' });
+                    i = 0;
+                }
             }
             Console.WriteLine("Числа ввведены, выберите нужное из доступных. X - выход");
             int[] choosenNumbers = new int[value];
-            int k = 0;
-            while (Console.ReadLine() != "X")
+            
+            int counter = 0;
+            string inputNumbers = "";
+            while (inputNumbers != "X")
             {
-                string j = Console.ReadLine();
-                choosenNumbers[k] = convertedNumbers[Convert.ToInt32(j)];
-                k++;
+                inputNumbers = Console.ReadLine();
+                    if (inputNumbers != "X")
+                    {
+                         bool secondSuccess = int.TryParse(inputNumbers, out int newInputNumbers);
+                         if (secondSuccess && newInputNumbers < value && newInputNumbers >= 0)
+                         {
+                              choosenNumbers[counter] = convertedNumbers[newInputNumbers];
+                              counter++;
+                         }
+                         else
+                         {
+                             Console.WriteLine("Неверно введён индекс массива: введите существующий индекс массива");
+                         }
+                    }
             }
             Console.WriteLine("Числа выбраны, введите необходимое действие");
             string actions = Console.ReadLine();
             int total = choosenNumbers[0];
             if (actions == "+")
             {
-                for (i = 1; i <= k; i++)
+                for (i = 1; i <= counter; i++)
                 {
                     total += choosenNumbers[i];
                 }
             }
             else if (actions == "-")
             {
-                for(i = 1; i <= k; i++)
+                for(i = 1; i <= counter; i++)
                 {
                     total -= choosenNumbers[i];
                 }
             }
             else if (actions == "/")
             {
-                for (i = 1; i <= k; i++)
+                for (i = 1; i <= counter; i++)
                 {
+                    if (choosenNumbers[i] == 0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя");
+                    }
                     total /= choosenNumbers[i];
                 }
             }
             else if (actions == "*")
             {
-                for (i = 1; i <= k; i++)
+                for (i = 1; i <= counter; i++)
                 {
                     total *= choosenNumbers[i];
                 }
