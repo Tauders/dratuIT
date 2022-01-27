ff5c82ab2f6339d7b52a038fee54daf4b6b8841f
Shair <hfphsds@gmail.com>
2022-01-28 02:18:26 +0300

    [Subtask -1] Первичная реализация

--- Subtasks/Dmitry/Subtask1/Subtask1/Program.cs
+++ Subtasks/Dmitry/Subtask1/Subtask1/Program.cs
@@ -6,7 +6,58 @@ namespace Subtask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите количество строк, которые хотите использовать");
+            int numberLines;
+            numberLines = Convert.ToInt32(Console.ReadLine());
+           
+           
+                string[] lines = new string[numberLines];
+            for (int i = 0; i < numberLines; i++)
+            {
+                Console.WriteLine("Введите строку");
+                string input = Console.ReadLine();
+                lines[i] = input;
+                
+            }
+            bool x = true;
+            while (x)
+
+            {
+                Console.WriteLine("Все строки введены, выберите строку для отображения");
+                string arrayIndex = Console.ReadLine();
+                int numberArrayIndext = 0;
+                if (arrayIndex == "x")
+                {
+                    x = false;
+                }
+                else
+                {
+                    numberArrayIndext = Convert.ToInt32(arrayIndex);
+                    Console.WriteLine(lines[numberArrayIndext]);
+                }
+                    
+            }
+                    
+
+
+            
+            
+             
+
+            
+            
+                
+            
+            
+
+            
+
+
+
+
+
+
+
         }
     }
 }
