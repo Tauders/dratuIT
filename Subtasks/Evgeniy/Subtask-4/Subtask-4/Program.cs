0a6d9203adfc5094055c6259e86ac92c040ef501
Evgeniy <uon.St1ke@mail.ru>
2022-01-30 04:15:42 +0700

    [Subtask-4]WIP Начало проекта

---
+++ Subtasks/Evgeniy/Subtask-4/Subtask-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subtask_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            List<string> listOfPeopleBySportsSection = new List<string>();
+            while (userInput.ToLower() != "exit")
+            {
+                userInput = Console.ReadLine();
+                if (userInput.ToLower() != "exit")
+                {
+                    listOfPeopleBySportsSection.Add(userInput);
+                }
+            }
+            Console.WriteLine("Данные введены, выберите способ сортировки");
+            int count = 0;
+            string[] words = new string[listOfPeopleBySportsSection.Count];
+            foreach (string choiceOfSectionByEachPerson in listOfPeopleBySportsSection)
+            {
+
+                words[count] = choiceOfSectionByEachPerson.Split(new char[] { ' ' });
+                count++;
+            }
+        }
+        
+    }
+}
