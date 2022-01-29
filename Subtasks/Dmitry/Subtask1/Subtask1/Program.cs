158b87159339fd8cb3e23c7f2a4b6074f9fded42
Shair <hfphsds@gmail.com>
2022-01-30 00:49:37 +0300

    [Subtask - 1] Пробная версия обработки ошибок и Try.Parse

--- Subtasks/Dmitry/Subtask1/Subtask1/Program.cs
+++ Subtasks/Dmitry/Subtask1/Subtask1/Program.cs
@@ -7,35 +7,46 @@ namespace Subtask1
         static void Main(string[] args)
         {
             Console.WriteLine("Введите количество строк, которые хотите использовать");
-            int numberLines;
+            /*int numberLines;
             numberLines = Convert.ToInt32(Console.ReadLine());
-           
-           
+            */
+            string strLines = Console.ReadLine();
+            int numberLines;
+            bool result = int.TryParse(strLines, out numberLines);
+            if (result)
+            {
+                int numberingLines = 1;
                 string[] lines = new string[numberLines];
-            for (int i = 0; i < numberLines; i++)
-            {
-                Console.WriteLine("Введите строку");
-                string input = Console.ReadLine();
-                lines[i] = input;
-                
-            }
-            bool x = true;
-            while (x)
+                for (int i = 0; i < numberLines; i++)
+                {
+                    Console.WriteLine("Введите строку " + (numberingLines++));
+                    string input = Console.ReadLine();
+                    lines[i] = input;
 
+                }
+                bool x = true;
+                while (x)
+
+                {
+                    Console.WriteLine("Все строки введены, выберите строку для отображения");
+                    string arrayIndex = Console.ReadLine();
+                    int numberArrayIndext = 0;
+                    if (arrayIndex == "x")
+                    {
+                        x = false;
+                    }
+                    else
+                    {
+                        numberArrayIndext = Convert.ToInt32(arrayIndex);
+                        Console.WriteLine(lines[numberArrayIndext]);
+                    }
+
+                }
+              
+            }
+            else
             {
-                Console.WriteLine("Все строки введены, выберите строку для отображения");
-                string arrayIndex = Console.ReadLine();
-                int numberArrayIndext = 0;
-                if (arrayIndex == "x")
-                {
-                    x = false;
-                }
-                else
-                {
-                    numberArrayIndext = Convert.ToInt32(arrayIndex);
-                    Console.WriteLine(lines[numberArrayIndext]);
-                }
-                    
+                Console.WriteLine("Ошибка! Нужно ввести число");
             }
                     
 
