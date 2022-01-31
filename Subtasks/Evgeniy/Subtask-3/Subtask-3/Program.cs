4ee083235a1af72571474543d1b296d104b1f831
Evgeniy <uon.St1ke@mail.ru>
2022-01-31 12:45:28 +0700

    [Subtask-3] Написал логику программы
    WIP Не хватает метода обратной сортировки

---
+++ Subtasks/Evgeniy/Subtask-3/Subtask-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subtask_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите построчно слова для дальнейшей сортировки");
+            string userInput = "";
+            List<string> aSetOfWords = new List<string>();
+            while (userInput.ToLower() != "exit")
+            {
+                userInput = Console.ReadLine();
+                if (userInput.ToLower() != "exit")
+                {
+                    aSetOfWords.Add(userInput);
+                }
+            }
+            int count = 0;
+            string[] words = new string[aSetOfWords.Count];
+            foreach (string aSetOfWord in aSetOfWords)
+            {
+                words[count] = aSetOfWords[count];
+                count++;
+            }
+            Console.WriteLine("Введите asc или desc для выбора сортировки");
+            string sortSelection = Console.ReadLine();
+            if (sortSelection.ToLower() == "asc")
+            {
+                Array.Sort(words);
+                Array.ForEach(words, Console.WriteLine);
+            }
+        }
+    }
+}
