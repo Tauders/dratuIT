e686c8417075e6f7cdc99e42d02d6ab52cdfa5a2
morgunovdv <morgunovdv3@hotmail.com>
2022-02-03 16:49:05 +0700

    Denis/subtask 4 (#28)
    * [Subtask - 4] Начало новой задачи
    
    * [Subtask - 4] Формирую основу задачи
    
    * [Subtask - 3] Отправляю домой
    
    * [Subtask - 4] Перевёл проект на .net5
    
    * [Subtask - 4]  Создаю основу задачи
    
    * [Subtask - 4] Всё еще создаю каркас программы
    
    * [Subtask - 4] Мелкие изменения
    
    * [Subtask - 4] Отправка домой
    
    * [Subtask - 4] Сделал заполняемость словаря
    
    * [Subtask - 4] Форматирование кода
    
    * [Subtask - 4] Добавил сортировку
    
    * [Subtask - 4] Примерный вариант метода
    
    * [Subtask - 4] Сделал метод сортировки
    
    * [Subtask - 4] Переписал заполняемость словаря
    
    * [Subtask - 4] Переименовал метод
    
    * [Subtask - 4] Всё корректировки
    
    * [Subtask - 4] Переписал условия проверки на X
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Набросок формы обработки исключений
    
    * [Subtask - 4] Внес корректировки
    
    * [Subtask - 4]  Написал обработку исклчений
    
    * [Subtask - 4] Исправление недочетов
    
    * [Subtask - 4] Сделал проверку на правильность выбора метода сортировки
    
    * [Subtask - 4] При не верном вводе методе предлагает ввести метод повторно
    
    * [Subtask - 4] Небольшое форматирование текста
    
    * [Subtask - 4] Переделал выбор сортировки с использованием сортировки
    
    * [Subtask - 4] Отформатировал текст
    
    * [Subtask - 4] Переписал метод
    
    * [Subtask - 4] Перепись метода версия 2.0
    
    * [Subtask - 4] Сохранение для отправки на работу
    
    * [Subtask - 4] Просто сохраняю свои попытки в сеть)
    
    * [Subtask - 4] Сделал повторный ввод при указании не верного метода
    
    * [Subtask - 4] Разделил на 2 файла. Отправляю решение домой
    
    * [Subtask - 4] Сделал проверку на наличие метода сортировки.
    
    * [Subtask - 4] Сделал проверку на Undefined и повторный ввод
    
    * [Subtask - 4] Работа над ошибками
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Исправление ошибок
    
    * [Subtask - 4] Написал обработку циферных значений перечисления. Но всё равно в дефолт на заходит.
    
    * [Subtask - 4] Переделал обработку перечисления.
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Добавил обработку не корректного ввода данных.
    
    * [Subtask - 4] Пересылка кода домой)
    
    * [Subtask - 4] Коррекция кода
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Корректровка кода
    
    * [Subtask - 4] Корректировка кода
    
    * [Subtask - 4] Списал готовое решение ошибки
    
    * [Subtask - 4] Корректировка ошибок
    
    * [Subtask - 4] Возможно финальные правки
    
    * [Subtask - 4] Переименовал переменную массива

---
+++ Subtasks/Denis/Subtask-4/Subtask-4/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Subtask_4
+{
+    class Program
+    {
+        public static void SortKeyValueValues(SortingMethod method, KeyValuePair<string, List<string>> groups)
+        {
+            switch (method)
+            {
+                case SortingMethod.Asc:
+                    groups.Value.Sort();
+                    break;
+                case SortingMethod.Desc:
+                    groups.Value.Sort();
+                    groups.Value.Reverse();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"Метод SortKeyValueValues не поддерживает значение Enum`a {method}");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> groups = new Dictionary<string, List<string>>();
+            bool isInputTermination = false;
+            Console.WriteLine("Введите данные в формате \"имя группа\", имя и группа  - одно слово ");
+            while (!isInputTermination)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Вы ничего не ввели");
+                }
+                else
+                {
+                    if (string.Equals(input, "x", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        isInputTermination = true;
+                    }
+                    else
+                    {
+                        string[] keyValuePair = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                         
+                        if (keyValuePair.Length != 2)
+                        {
+                            Console.WriteLine("Допушена ошибка при вводе");
+                        }
+                        else
+                        {
+                            string name = keyValuePair[0];
+                            string group = keyValuePair[1];
+                            if (!groups.ContainsKey(group))
+                            {
+                                groups.Add(group, new List<string>());
+                            }
+                            groups[group].Add(name);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Выберите метод сортировки:");
+            Console.WriteLine("1. Asc");
+            Console.WriteLine("2. Desc");
+            SortingMethod sorting = SortingMethod.Undefined;
+            while (sorting == SortingMethod.Undefined)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input)) 
+                {
+                    Console.WriteLine("Вы ничего не ввели");
+                }
+                else
+                {
+                    if (Enum.TryParse<SortingMethod>(input, true, out sorting))
+                    {
+                        if (!Enum.IsDefined(typeof(SortingMethod), sorting))
+                        {
+                            sorting = SortingMethod.Undefined;
+                        }
+                    }
+                }
+                
+                if(sorting == SortingMethod.Undefined)
+                {
+                    Console.WriteLine("Данного метода не существует. Повторите ввод");
+                }
+            }
+
+            foreach (KeyValuePair<string, List<string>> group in groups)
+            {
+                Console.WriteLine($"Группа {group.Key}");
+                SortKeyValueValues(sorting, group);
+                foreach (string value in group.Value)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Subtasks/Denis/Subtask-4/Subtask-4/SortingMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace Subtask_4
+{
+    public enum SortingMethod
+    {
+        Undefined = 0,
+        Asc = 1,
+        Desc = 2
+    }
+}
