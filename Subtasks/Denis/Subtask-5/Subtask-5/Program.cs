1c00f467454812a2fe3135cf177fa62cda2077c2
Denis Morgunov <morgunovdv3@hotmail.com>
2022-02-03 13:05:55 +0700

    [Subtask - 5] Написал общую концепцию программы

--- Subtasks/Denis/Subtask-5/Subtask-5/Program.cs
+++ Subtasks/Denis/Subtask-5/Subtask-5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Subtask_5
 {
@@ -6,7 +7,33 @@ namespace Subtask_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Random rnd = new Random();
+            int numbersLenth = rnd.Next(10, 1001);
+            int[] numbers = new int[numbersLenth];
+            int min = 100;
+            int max = 0;
+            double middle = 0;
+            double result = 0;
+            for (int i = 0; i < numbersLenth; i++)
+            {
+                numbers[i] = rnd.Next(1, 101);
+                int temp = numbers[i];
+                Console.Write(numbers[i] + " ");
+                if (min > temp)
+                {
+                    min = temp;
+                }
+                else if (max < temp)
+                {
+                    max = temp;
+                }
+                result += numbers[i] ;
+            }
+            middle = result / numbersLenth;
+
+            Console.WriteLine($"\nМинимальное значение: {min}, Максимальное значение: {max}, Среднее значение {middle}");
+            Console.ReadLine();
+
+        } 
     }
 }
