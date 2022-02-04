a9fa4fc21a67db7a4df436047d5b360f570cba6e
Denis Morgunov <morgunovdv3@hotmail.com>
2022-02-04 14:22:10 +0700

    [Subtask - 5] Финальная версия программы

--- Subtasks/Denis/Subtask-5/Subtask-5/Program.cs
+++ Subtasks/Denis/Subtask-5/Subtask-5/Program.cs
@@ -10,45 +10,64 @@ namespace Subtask_5
         {
             Console.WriteLine("Введите количество чисел (от 10 до 1000)");
             Random rnd = new Random();
-            string inputNumberLenth = Console.ReadLine();
-            if(int.TryParse(inputNumberLenth, out int numbersLenth))
+            bool isSelect = false;
+            while (!isSelect)
             {
-                List<int> numbers = new List<int>();
-                Console.WriteLine("Количество чисел получено");
-                int min = 100;
-                int max = 0;
-                double result = 0;
-                for (int i = 0; i < numbersLenth; i++)
+                string inputNumberLenth = Console.ReadLine();
+                if (int.TryParse(inputNumberLenth, out int numbersLenth))
                 {
-                    numbers.Add(rnd.Next(1, 101));
-                    int tempNumber = numbers[i];
-                    if (min > tempNumber)
+                    if ((numbersLenth < 10) || (numbersLenth > 1000))
                     {
-                        min = tempNumber;
+                        Console.WriteLine("Введенное вами число вне диапозона");
                     }
-                    else if (max < tempNumber)
+                    else
                     {
-                        max = tempNumber;
+                        List<int> numbers = new List<int>();
+                        Console.WriteLine("Количество чисел получено");
+                        int max = 0;
+                        double result = 0;
+                        for (int i = 0; i < numbersLenth; i++)
+                        {
+                            numbers.Add(rnd.Next(1, 101));
+                            int tempNumber = numbers[i];
+                            if (max < tempNumber)
+                            {
+                                max = tempNumber;
+                            }
+
+                            result += numbers[i];
+                            Console.Write(numbers[i] + " ");
+                        }
+
+                        int min = max;
+                        for (int i = 0; i < numbersLenth; i++)
+                        {
+                            int tempNumber = numbers[i];
+                            if (min > tempNumber)
+                            {
+                                min = tempNumber;
+                            }
+                        }
+                        isSelect = true;
+
+                        IEnumerable<int> distinctNumbers = numbers.Distinct();
+                        double midValue = Math.Round((result / numbersLenth), 1);
+                        Console.WriteLine("\nУникальные значения:");
+                        foreach (int number in distinctNumbers)
+                        {
+                            Console.Write(number + " ");
+                        }
+                        Console.WriteLine($"\nМинимально значение = {min}");
+                        Console.WriteLine($"Максимальное значение = {max}");
+                        Console.WriteLine($"Среднее значение = {midValue}");
                     }
-                    result += numbers[i];
-                    Console.Write(numbers[i] + " ");
-                }
-                IEnumerable<int> distinctNumbers = numbers.Distinct();
-                double midValue = Math.Round((result / numbersLenth), 1);
-                Console.WriteLine("\nУникальные значения:");
-                foreach (int number in distinctNumbers)
+                } 
+                else
                 {
-                    Console.Write(number + " ");
+                    Console.WriteLine("Вы указали данные в другом формате");
                 }
-                Console.WriteLine($"\nМинимально значение = {min}");
-                Console.WriteLine($"Максимальное значение = {max}");
-                Console.WriteLine($"Среднее значение = {midValue}");
-            }
-            else
-            {
-                Console.WriteLine("Вы указали данные в другом формате");
             }
             Console.ReadLine();
-        } 
+        }
     }
 }
