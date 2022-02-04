07b601a9b59612d46be2414495a93041009816c5
Evgeniy <uon.St1ke@mail.ru>
2022-02-05 04:06:54 +0700

    [Subtask-3] Поработал над вашими ревью:
    1)Переименовал переменные
    2)Добавил использование switch/case
    WIP; Enum и повторение кода

--- Subtasks/Evgeniy/Subtask-3/Subtask-3/Program.cs
+++ Subtasks/Evgeniy/Subtask-3/Subtask-3/Program.cs
@@ -8,42 +8,48 @@ namespace Subtask_3
         static void Main(string[] args)
         {
             Console.WriteLine("Введите построчно слова для дальнейшей сортировки");
-            string userInput = "";
-            List<string> SetOfWords = new List<string>();
-            bool validUserInput = false;
-            while (!validUserInput)
+            List<string> setOfWords = new List<string>();
+            bool isInputEnded = false;
+            while (!isInputEnded)
             {
-                userInput = Console.ReadLine();
+                string userInput = Console.ReadLine();
                 if (userInput.ToLower() != "exit")
                 {
-                    SetOfWords.Add(userInput);
+                    setOfWords.Add(userInput);
                 }
                 else
                 {
-                    validUserInput = true;
+                    isInputEnded = true;
                 }
             }
-            Console.WriteLine("Введите asc или desc для выбора сортировки");
-            string sortSelection = Console.ReadLine();
-            if (sortSelection.ToLower() == "asc")
+            bool endOfInput = false;
+            while (!endOfInput)
             {
-                SetOfWords.Sort();
-                foreach (var word in SetOfWords)
+                Console.WriteLine("Введите asc или desc для выбора сортировки");
+                string sortSelection = Console.ReadLine().ToLower();
+                switch (sortSelection)
                 {
-                    Console.WriteLine(word);
+                    case "asc":
+                        setOfWords.Sort();
+                        foreach (var word in setOfWords)
+                        {
+                            Console.WriteLine(word);
+                        }
+                        break;
+                    case "desc":
+                        setOfWords.Sort((str1, str2) => { return string.Compare(str2, str1); });
+                        foreach (var word in setOfWords)
+                        {
+                            Console.WriteLine(word);
+                        }
+                        break;
+                    case "exit":
+                        endOfInput = true;
+                        break;
+                    default:
+                        Console.WriteLine("Вы выбрали неверный способ сортировки, попробуйте ещё раз");
+                        break;
                 }
-            }
-            else if (sortSelection.ToLower() == "desc")
-            {
-                SetOfWords.Sort((str1, str2) => { return string.Compare(str2, str1); });
-                foreach (var word in SetOfWords)
-                {
-                    Console.WriteLine(word);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Вы ввели неверное значение, значит вам нужно отдохнуть и больше не вводить значений");
             }
         }
     }
