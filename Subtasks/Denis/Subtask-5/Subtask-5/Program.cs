1799842d958295c0e68315bc2cd357f284968de1
Денис Моргунов <Morgunovdv3@hotmail.com>
2022-02-06 13:53:09 +0700

    [Subtask - 5] Указал булевое значение без отричания

--- Subtasks/Denis/Subtask-5/Subtask-5/Program.cs
+++ Subtasks/Denis/Subtask-5/Subtask-5/Program.cs
@@ -9,9 +9,9 @@ namespace Subtask_5
         {
             Console.WriteLine("Введите количество чисел (от 10 до 1000)");
             Random randomNumber = new Random();
-            bool isError = false;
+            bool isError = true;
             int numbersLength = 0;
-            while (!isError)
+            while (isError)
             {
                 string inputNumberLength = Console.ReadLine();
                 if (int.TryParse(inputNumberLength, out numbersLength))
@@ -22,7 +22,7 @@ namespace Subtask_5
                     }
                     else
                     {
-                        isError = true;
+                        isError = false;
                     }
                 } 
                 else
