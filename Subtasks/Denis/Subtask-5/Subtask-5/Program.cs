c07522f25ee0a7067c6cf9e8354f6e07bcf12daf
morgunovdv <morgunovdv3@hotmail.com>
2022-02-06 21:43:03 +0700

    Denis/subtask 5 (#32)
    * [Subtask - 5] Подготовка ветки для новой задачи
    
    * [Subtask - 5] Написал общую концепцию программы
    
    * [Subtask - 5] Добавил вывод текста на консоли
    
    * [Subtask - 5] Перевел задачу с массива на список
    
    * [Subtask - 5] Задача на проверку
    
    * [Subtask - 5] Финальная версия программы
    
    * [Subtask - 5] Альфа версия программы для проверки.
    
    * [Subtask - 5] Переписал код под условия задачи.
    
    * [Subtask - 5] Переименовал переменные.
    
    * [Subtask - 5] Исправление ошибок
    
    * [Subtask - 5] Сменил название переменных в методах поиска min и max
    
    * [Subtask - 5] Уменьшил цикл while
    
    * [Subtask - 5] Переименовал объект класса Random. Написал вычисление max значения
    
    * [Subtask - 5] Написал вычисления для нахождения минимального значения
    
    * [Subtask - 5] Задал переменной min максимальное значение и упростил проверку
    
    * [Subtask - 5] Указал булевое значение без отричания
    
    * [Subtask - 5] Исправил орфографическую ошибку
    
    * [Subtask - 5] Изменил тип данных переменной result
    
    * [Subtask - 5] Переименовал булевую переменную

---
+++ Subtasks/Denis/Subtask-5/Subtask-5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subtask_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел (от 10 до 1000)");
+            Random randomNumber = new Random();
+            bool isUnknownNumber = true;
+            int numbersLength = 0;
+            while (isUnknownNumber)
+            {
+                string inputNumberLength = Console.ReadLine();
+                if (int.TryParse(inputNumberLength, out numbersLength))
+                {
+                    if ((numbersLength < 10) || (numbersLength > 1000))
+                    {
+                        Console.WriteLine("Введенное вами число вне диапазона");
+                    }
+                    else
+                    {
+                        isUnknownNumber = false;
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("Вы указали не числовое значение. Повторите ввод");
+                }
+            }
+            HashSet<int> numbers = new HashSet<int>();
+            Console.WriteLine("Количество чисел получено");
+            for (int i = 0; i < numbersLength; i++)
+            {
+                numbers.Add(randomNumber.Next(1, 101));
+            }
+
+            int max = 0;
+            int min = int.MaxValue;
+            int result = 0;
+            Console.WriteLine("Уникальные значения:");
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+                if (max < number)
+                    max = number;
+
+                if (min > number)
+                    min = number;
+
+                result += number;
+            }
+
+            double midNumber = Math.Round(((double)result / numbers.Count), 1);
+            Console.WriteLine();
+            Console.WriteLine($"Минимальное значение = {min}");
+            Console.WriteLine($"Максимальное значение значение = {max}");
+            Console.WriteLine($"Среднее значение = {midNumber}");
+
+            Console.ReadLine();
+        }
+    }
+}
