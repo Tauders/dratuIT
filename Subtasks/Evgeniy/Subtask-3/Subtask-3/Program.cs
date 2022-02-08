f6c97a1327bc6f36fcda3dc887ec7328923dea59
Evgeniy <uon.St1ke@mail.ru>
2022-02-08 14:20:37 +0700

    [Subtask-3] Перенёс enum в отдельный файл

--- Subtasks/Evgeniy/Subtask-3/Subtask-3/Program.cs
+++ Subtasks/Evgeniy/Subtask-3/Subtask-3/Program.cs
@@ -54,11 +54,6 @@ namespace Subtask_3
             }
 
         }
-        enum SortMethod
-        {
-            Unknown = 0,
-            Asc = 1,
-            Desc = 2
-        }
+        
     }
 }
---
+++ Subtasks/Evgeniy/Subtask-3/Subtask-3/SortMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace Subtask_3
+{
+    enum SortMethod
+    {
+        Unknown = 0,
+        Asc = 1,
+        Desc = 2
+    }
+}
