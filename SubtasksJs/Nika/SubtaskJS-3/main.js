55c2ceb896da9aac5627c16768f630f6ef668741
VeronikaFront <veronika.korolkova99@gmail.com>
2022-08-17 22:52:36 +0700

    Упражнение 2 - удалила лишние файлы

--- SubtasksJs/Nika/SubtaskJS-3/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".form").addEventListener('submit', function(e) {
-    let passValue = document.querySelector('.form_data').value;
-    console.log("Тюлень ввёл : ", passValue);
-    document.querySelector('.form_data').value = '';
-});
