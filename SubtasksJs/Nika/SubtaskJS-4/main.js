630822597b28d811e86de494408898e4fb62020d
VeronikaFront <veronika.korolkova99@gmail.com>
2022-08-29 23:07:12 +0700

    коррекция

--- SubtasksJs/Nika/SubtaskJS-4/main.js
+++ SubtasksJs/Nika/SubtaskJS-4/main.js
@@ -1,17 +1,22 @@
 const formInputNumberID = 'input-number--form';
 const formInputNumber = document.getElementById(formInputNumberID);
+
 const enteredValueID = 'form--input-data';
 const inputResultBlockID = 'results--input';
+const inputResultBlock = document.getElementById(inputResultBlockID);
+
 const randomNumberGenerationResultBlockID = 'results--generation';
 const resultCalculatingArithmeticMeanBlockID =
   'result--calculating-arithmetic-mean';
-const resultCalculationsMinimumBlockID = 'result--calculations-minimum';
-const resultCalculationsMaximumBlockID = 'result--calculations-maximum';
-const arrayRandomlyGeneratedNumbers = [];
+const resultCalculationsMinimumBlockID = 'result--calculating-minimum';
+const resultCalculationsMaximumBlockID = 'result--calculating-maximum';
+
 const errorBoxID = 'errorBox';
 const errorBox = document.getElementById(errorBoxID);
-const inputResultBlock = document.getElementById(inputResultBlockID);
 
+const resultCalculationsMaximumBlock = document.getElementById(
+  resultCalculationsMaximumBlockID
+);
 const randomNumberGenerationResultBlock = document.getElementById(
   randomNumberGenerationResultBlockID
 );
@@ -20,9 +25,6 @@ const resultCalculatingArithmeticMeanBlock = document.getElementById(
 );
 const resultCalculationsMinimumBlock = document.getElementById(
   resultCalculationsMinimumBlockID
-);
-const resultCalculationsMaximumBlock = document.getElementById(
-  resultCalculationsMaximumBlockID
 );
 
 function createError(text) {
@@ -34,10 +36,86 @@ function createError(text) {
   }
 }
 
-function clearErrorboxSheet() {
+function clearErrorbox() {
   if (errorBox.firstChild) {
     errorBox.innerHTML = '';
   }
+}
+
+function validateEnteredValue(enteredValue) {
+  clearErrorbox();
+  if (!isFinite(enteredValue)) {
+    createError('Необходимо ввести одно число!');
+    return false;
+  }
+  if (enteredValue % 1 != 0) {
+    createError('Необходимо ввести целое число!');
+    return false;
+  }
+  if (enteredValue < 10 || enteredValue > 1000) {
+    createError('Необходимо ввести число в диапазоне [10; 1000]');
+    return false;
+  }
+  return true;
+}
+
+function generateRandomInteger() {
+  return Math.ceil(100 * Math.random());
+}
+
+function createArrayRandomlyGeneratedNumbers(enteredValue) {
+  const arrayRandomlyGeneratedNumbers = [];
+  for (let i = 0; i < enteredValue; i++) {
+    arrayRandomlyGeneratedNumbers.push(generateRandomInteger());
+  }
+  return arrayRandomlyGeneratedNumbers;
+}
+
+function clearArrayRandomlyGeneratedNumbers(arrayRandomlyGeneratedNumbers) {
+  arrayRandomlyGeneratedNumbers.length = 0;
+}
+
+function createStringOfArrayElementsWithDelimiter(
+  arrayRandomlyGeneratedNumbers
+) {
+  return arrayRandomlyGeneratedNumbers.join(' ');
+}
+
+function outputRandomlyGeneratedNumbers(arrayRandomlyGeneratedNumbers) {
+  randomNumberGenerationResultBlock.innerHTML =
+    createStringOfArrayElementsWithDelimiter(arrayRandomlyGeneratedNumbers);
+}
+
+function additionRandomlyGeneratedNumbers(arrayRandomlyGeneratedNumbers) {
+  return arrayRandomlyGeneratedNumbers.reduce((sum, current) => sum + current);
+}
+function calculateArithmeticMeanNumbers(arrayRandomlyGeneratedNumbers) {
+  return additionRandomlyGeneratedNumbers(arrayRandomlyGeneratedNumbers) / 2;
+}
+
+function outputResultCalculatingArithmeticMean(arrayRandomlyGeneratedNumbers) {
+  resultCalculatingArithmeticMeanBlock.innerHTML =
+    calculateArithmeticMeanNumbers(arrayRandomlyGeneratedNumbers);
+}
+
+function calculateMinimumNumbers(arrayRandomlyGeneratedNumbers) {
+  return Math.min(...arrayRandomlyGeneratedNumbers);
+}
+
+function calculateMaximumNumbers(arrayRandomlyGeneratedNumbers) {
+  return Math.max(...arrayRandomlyGeneratedNumbers);
+}
+
+function outputResultCalculationsMinimum(arrayRandomlyGeneratedNumbers) {
+  resultCalculationsMinimumBlock.innerHTML = calculateMinimumNumbers(
+    arrayRandomlyGeneratedNumbers
+  );
+}
+
+function outputResultCalculationsMaximum(arrayRandomlyGeneratedNumbers) {
+  resultCalculationsMaximumBlock.innerHTML = calculateMaximumNumbers(
+    arrayRandomlyGeneratedNumbers
+  );
 }
 
 function clearBlocksResults() {
@@ -48,82 +126,33 @@ function clearBlocksResults() {
   inputResultBlock.innerHTML = '';
 }
 
-function createArrayRandomlyGeneratedNumbers(enteredValue) {
-  for (let i = 0; i < enteredValue; i++) {
-    arrayRandomlyGeneratedNumbers.push(generateRandomInteger());
-  }
-}
-
-function clearArrayRandomlyGeneratedNumbers() {
-  arrayRandomlyGeneratedNumbers.length = 0;
-}
-
-function outputRandomlyGeneratedNumbers() {
-  randomNumberGenerationResultBlock.innerHTML =
-    arrayRandomlyGeneratedNumbers.join(' ');
-}
-
-function generateRandomInteger() {
-  return Math.ceil(100 * Math.random());
-}
-
-function calculateArithmeticMeanNumbers() {
-  const additionRandomlyGeneratedNumbers = arrayRandomlyGeneratedNumbers.reduce(
-    (sum, current) => sum + current
-  );
-  return additionRandomlyGeneratedNumbers / 2;
-}
-
-function calculateMinimumNumbers() {
-  return Math.min(...arrayRandomlyGeneratedNumbers);
-}
-
-function calculateMaximumNumbers() {
-  return Math.max(...arrayRandomlyGeneratedNumbers);
-}
-
-function outputResultCalculatingArithmeticMean() {
-  resultCalculatingArithmeticMeanBlock.innerHTML =
-    calculateArithmeticMeanNumbers();
-}
-
-function outputResultCalculationsMinimum() {
-  resultCalculationsMinimumBlock.innerHTML = calculateMinimumNumbers();
-}
-
-function outputResultCalculationsMaximum() {
-  resultCalculationsMaximumBlock.innerHTML = calculateMaximumNumbers();
-}
-
-function checkEnteredValue(enteredValue) {
-  if (!isFinite(enteredValue)) {
-    createError('Необходимо ввести одно число!');
-    clearBlocksResults();
-  } else if (enteredValue < 10 || enteredValue > 1000) {
-    createError('Необходимо ввести число в диапазоне [10; 1000]');
-    clearBlocksResults();
-  } else {
-    return true;
-  }
+function outputInputResult(enteredValue) {
+  inputResultBlock.innerHTML = enteredValue;
 }
 
 formInputNumber.addEventListener('submit', function (e) {
   e.preventDefault();
   const enteredValue = document.getElementById(enteredValueID).value;
 
-  if (checkEnteredValue(enteredValue) != true) {
-    return;
+  if (!validateEnteredValue(enteredValue)) {
+    clearBlocksResults();
+    document.getElementById(enteredValueID).value = '';
+    return false;
   }
 
-  inputResultBlock.innerHTML = enteredValue;
+  clearErrorbox();
+  outputInputResult(enteredValue);
 
-  clearErrorboxSheet();
   createArrayRandomlyGeneratedNumbers(enteredValue);
-  outputRandomlyGeneratedNumbers();
-  outputResultCalculatingArithmeticMean();
-  outputResultCalculationsMinimum();
-  outputResultCalculationsMaximum();
+
+  const arrayRandomlyGeneratedNumbers =
+    createArrayRandomlyGeneratedNumbers(enteredValue);
+
+  outputRandomlyGeneratedNumbers(arrayRandomlyGeneratedNumbers);
+  outputResultCalculatingArithmeticMean(arrayRandomlyGeneratedNumbers);
+  outputResultCalculationsMinimum(arrayRandomlyGeneratedNumbers);
+  outputResultCalculationsMaximum(arrayRandomlyGeneratedNumbers);
 
   document.getElementById(enteredValueID).value = '';
-  clearArrayRandomlyGeneratedNumbers();
+  clearArrayRandomlyGeneratedNumbers(arrayRandomlyGeneratedNumbers);
 });
